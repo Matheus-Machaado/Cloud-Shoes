dc750f231a6ef0da229822018618303c0868d9b5
Matheus Henrique Machado <matheus.thosi@gmail.com>
2023-11-05 16:32:24 -0300

    inicial

---
+++ cloud.js
@@ -0,0 +1,20 @@
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink() {
+    menuItem.forEach((item) =>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
+)
+
+function openMenu() {
+    document.getElementById('navbarlateral').style.width = '84vw';
+}
+
+function fecharMenu() {
+    document.getElementById('navbarlateral').style.width = '0';
+}
